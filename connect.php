086f1a118d54b4f8e6fc97b0ec2685e530f2ca0c
Med2b <aznoggfr@gmail.com>
2024-01-03 08:44:47 +0000

    remove nev

--- connect.php
+++ connect.php
@@ -1,11 +1,46 @@
 <?php
 
-session_start();
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  session_start();
+  // Generate a random token
+  $token = bin2hex(random_bytes(32));
+  // Store the token in the session
+  $_SESSION['id'] = $token;
+  header('Location: /project-bd/');
+}
 
-// Generate a random token
-$token = bin2hex(random_bytes(32));
+?>
 
-// Store the token in the session
-$_SESSION['id'] = $token;
+<!doctype html>
+<html>
 
-header('Location: /project-bd/');
+<head>
+  <?php require_once "src/components/head.php" ?>
+</head>
+
+<body>
+  <main class="my-10">
+    <div class="container mx-auto px-5">
+      <div class="bg-white p-4 shadow rounded">
+        <h1 class="text-3xl font-semibold mb-4">Connexion</h1>
+        <form action="" method="post">
+          <div class="mb-4">
+            <label for="email" class="block mb-2">Adresse email</label>
+            <input type="text" name="email" id="email" value="admin" class="w-full border border-gray-300 rounded p-2"
+              required>
+          </div>
+          <div class="mb-4">
+            <label for="password" class="block mb-2">Mot de passe</label>
+            <input type="password" name="password" id="password" value="admin"
+              class="w-full border border-gray-300 rounded p-2" required>
+          </div>
+          <div class="mb-4">
+            <button type="submit" class="bg-blue-500 text-white rounded p-2">Se connecter</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </main>
+</body>
+
+</html>
--- src/components/header.php
+++ src/components/header.php
@@ -3,11 +3,14 @@
     <a href="/project-bd/">
       <h1 class="text-xl font-semibold">ASCG Dashboard</h1>
     </a>
-    <div class="ml-auto flex">
+    <div class="ml-auto flex items-center">
       <a href="/project-bd/adherents/" class="text-zinc-500 hover:text-zinc-600 mr-4">Adhérants</a>
       <a href="/project-bd/benevoles/" class="text-zinc-500 hover:text-zinc-600 mr-4">Bénévoles</a>
       <a href="/project-bd/sections/" class="text-zinc-500 hover:text-zinc-600 mr-4">Sections</a>
       <a href="/project-bd/activites/" class="text-zinc-500 hover:text-zinc-600 mr-4">Activités</a>
+      <?php if (isset($_SESSION['id'])): ?>
+      <a href="/project-bd/deconnect.php" class="p-1.5 bg-red-400 text-white">Déconnexion</a>
+      <?php endif; ?>
     </div>
   </div>
 </header>
